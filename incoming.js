082f8df3ba3ff08874b645dd65a4492ec1c760ad
FM <Matthew.Finkel@gmail.com>
2012-05-15 23:26:35 -0400

    Perform form validation client-side

--- incoming.js
+++ incoming.js
@@ -65,3 +65,70 @@ function checkFilled(where)
     loc.value = "Null";
   }
 }
+
+function verifyIncoming()
+{
+  var error = "";
+  var str = "";
+  if(document.forms["theForm"]["name"].value == "")
+    str += "[Name] ";
+  if(document.forms["theForm"]["cell1"].value == "")
+    str += "[Area Code] ";
+  if(document.forms["theForm"]["cell2"].value == "")
+    str += "[Beginning of Phone Number] ";
+  if(document.forms["theForm"]["cell3"].value == "")
+    str += "[End of Phone Number] ";
+  if(document.forms["theForm"]["riders"].value == "")
+    str += "[Number of passangers] ";
+  if(document.forms["theForm"]["fromloc"].value == "Null")
+  {
+    str += "[Pickup] ";
+  }
+  else  
+  {
+    if(document.forms["theForm"]["fromloc"].value == "Other")
+    {
+      if(document.forms["theForm"]["pickup"].value == "") 
+      {
+        str += "[Pickup Location] ";
+      }
+    }
+    else
+    {
+      if(document.forms["theForm"]["pickup"].value != "") 
+      {
+        error += "[Pickup has invalid value!] ";
+      }
+    }
+  }
+  if(document.forms["theForm"]["toloc"].value == "Null")
+  {
+    str += "[Dropoff] ";
+  }
+  else  
+  {
+    if(document.forms["theForm"]["toloc"].value == "Other")
+    {
+      if(document.forms["theForm"]["dropoff"].value == "") 
+      {
+        str += "[Dropoff Location] ";
+      }
+    }
+    else
+    {
+      if(document.forms["theForm"]["dropoff"].value != "") 
+      {
+        error += "[Dropoff has invalid value!] ";
+      }
+    }
+  }
+  if(error != "")
+    str = str + "\nErrors: " + error;
+  if(str != "")
+    alert("Please fill in the Following Field: " + str);
+  else
+  {
+    document.theForm.submit();
+  }
+
+}
