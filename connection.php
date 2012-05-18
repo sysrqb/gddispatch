4477b1a8f89a6803da6a58b2cf24696600a25c3c
FM <Matthew.Finkel@gmail.com>
2012-05-18 12:27:45 -0400

    Factored out connection element

--- connection.php
+++ connection.php
@@ -1,9 +1,38 @@
 <?php
 
-$con = new mysqli($host,$username,$password);
-if($con->connect_error){
-        die('Connection Error (' . $con->connect_errno . ') ' . $con->connect_error);
+function connect(){
+  global $host, $username, $password, $db, $prepare, $log, $logfile;
+
+  $con = new mysqli($host,$username,$password);
+  if($con->connect_error)
+  {
+    $error = 'Connection Error (' . $con->connect_errno . ') ' . $con->connect_error;
+    loganddie($error);
+  }
+  $con->select_db($db);
+  return $con;
 }
-$con->select_db($db);
+   
+
+function loginLdap($username, $password){
+	global $ldapserver,$localadmin;
+	global $auth;
+	if($username=='admin' && $password=='1234' && $localadmin==TRUE){
+		 config(TRUE);
+	}
+	/*else{
+		$conserver = ldap_connect($ldapserver['host']); //$ldapserver is defined in creds.php
+		$ldapbind = ldap_bind($conserveri,$username,$password)
+			or die('Could not establish connection with server: ' . $ldapserver['name'] . "\n");
+		echo "Bind Successful \n";
+		$auth=$TRUE;
+		header("location: ./config.php");
+		return 'location ./index.php';
+	}*/
+	else{
+		return 'location: ./index.php';
+	}
+}
+
 
 ?>
