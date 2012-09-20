e4eabca26973c893aa262fe59e53b0486f546ef7
FM <matthew.finkel@gmail.com>
2012-09-20 02:12:48 -0400

    Added copyright notices to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 <div class="footer">
 	<h6 style="text-align: center;"><?php echo date('Y'); ?> - <a href="mailto:IT.GUARDdogs@gmail.com">Your Loving IT Officer</a><!--Matthew Finkel--></h6>
 	</div>
+	<br />
+<div class="footer-copy">
+	<h6 style="text-align: center;">Phoneroom - Copyright (C) 2010 CARPOOL</h6>
+	<h6 style="text-align: center;">Dispatch - Copyright (C) 2012 GUARD Dogs
+	</div>
