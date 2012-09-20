201133e2075ae61a116c19a3d133cce0cacbc3e9
FM <matthew.finkel@gmail.com>
2012-09-20 02:20:34 -0400

    Example credentials file

---
+++ cred.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'xxx.xxx.xxx.xxx';
+$username = 'dbuser';
+$password = 'dbuserpasswd';
+$db = 'dbname';
+$port = 3306;
+$CASserver = 'https://login.example.com';
+$CASValidateServer = 'https://login.example.com/';
+$LocalAuthValidator = 'http://xxx.xxx.xxx.xxx/auth.php';
+
+/* Setting this to true will allow easy access to the administration
+   screen
+ */
+$localadmin = TRUE;
+?>
